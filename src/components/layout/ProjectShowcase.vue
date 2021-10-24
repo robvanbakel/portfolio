81675527717674b1e698004056979f7dfd4b33e2
Rob van Bakel <robbakel@gmail.com>
2021-10-24 12:37:38 +0200

    Remove hover effect on project images

--- src/components/layout/ProjectShowcase.vue
+++ src/components/layout/ProjectShowcase.vue
@@ -2,7 +2,7 @@
   <div class="project">
     <div class="project-image desktop">
       <router-link :to="{ name: project.name }">
-        <img :src="`/images/${project.images.showcase}`" :alt="`${project.name} Screenshot`" ref="showcaseImage" />
+        <img :src="`/images/${project.images.showcase}`" :alt="`${project.name} Screenshot`" />
       </router-link>
     </div>
     <div class="project-title">
@@ -53,30 +53,6 @@ export default {
         e.target.firstChild.classList.remove("expanded")
       }
     },
-  },
-  mounted() {
-    const image = this.$refs.showcaseImage
-
-    image.addEventListener("mousemove", (e) => {
-      const imageCenterX = image.getBoundingClientRect().x + image.offsetWidth / 2
-      const imageCenterY = image.getBoundingClientRect().y + image.offsetHeight / 2
-
-      const y = (imageCenterX - e.x) / -35
-      const x = (imageCenterY - e.y) / 35
-
-      image.style.transform = `scale(1.015) rotateX(${x}deg) rotateY(${y}deg)`
-      image.style.boxShadow = "0px 20px 100px rgba(0,0,0,0.1)"
-    })
-
-    image.addEventListener("mouseleave", (e) => {
-      image.style.transform = `scale(1) rotateX(0deg) rotateY(0deg)`
-      image.style.boxShadow = "0px 20px 60px rgba(0,0,0,0.1)"
-    })
-
-    image.addEventListener("mousedown", () => {
-      image.style.transform = `translateZ(-20px)`
-      image.style.boxShadow = "0px 0px 60px rgba(0,0,0,0.1)"
-    })
   },
 }
 </script>
