a043e6bfe86336e8af684aaba88fc38f57dad4fd
Rob van Bakel <robbakel@gmail.com>
2021-10-28 14:30:33 +0200

    Add small features section to GoTiny

---
+++ src/components/layout/FeatureSmall.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="feature">
+    <h3>
+      <slot name="title"></slot>
+    </h3>
+    <div class="feature-image-wrapper desktop">
+      <img :src="`/images/${img}`" :alt="this.$slots['title']()[0].children" />
+    </div>
+    <p>
+      <slot name="body"></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
